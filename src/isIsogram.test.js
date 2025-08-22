4f2b397b4dc20b3fc8eeba04a69f60259eb5c3a4
Dmytro Koriahin <dkoryahin@gmail.com>
2025-08-22 11:51:33 +0300

    add task solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,24 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`'true' when we have different letters`, () => {
+    expect(isIsogram('abcde')).toBe(true);
+  });
+
+  it(`'true' when we have empty sting`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`'false' when we have similar letters`, () => {
+    expect(isIsogram('damask')).toBe(false);
+  });
+
+  it(`'false' when we have similar letters with different case`, () => {
+    expect(isIsogram('Ana')).toBe(false);
+  });
+
+  it(`'false' when we have similar numbers`, () => {
+    expect(isIsogram('dima1991')).toBe(false);
+  });
 });
